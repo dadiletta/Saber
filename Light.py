50fcfe35e571ebe0981bd7332d9e2a44ba225441
Dan Adiletta <dan.adiletta@gmail.com>
2016-05-14 14:33:01 +0000

    Initial box assembly.

--- Light.py
+++ Light.py
@@ -1,40 +1,166 @@
+'''
+## License
+The MIT License (MIT)
+GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
+Copyright (C) 2015  Dexter Industries
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+
 from grovepi import *
+from grove_rgb_lcd import *
 import grovepi
-#import web
+import time
 
 
-# Connect the Grove LED to digital port D4
-led = 4
+# Configure buzzer on D4
+buzzer = 4
+grovepi.pinMode(buzzer,"OUTPUT")
 
-# Connect the Grove Ultrasonic Ranger to digital port D4
-# SIG,NC,VCC,GND
-ultrasonic_ranger = 5
+# Connect the Grove LED to digital port D7-8
+gled = 7
+rled = 8
+grovepi.pinMode(gled,"OUTPUT")
+grovepi.pinMode(rled,"OUTPUT")
 
+# Connect the Grove Ultrasonic Ranger to digital port D2
+ultrasonic_ranger = 2
+
+# Connect the Grove Button to digital port D3
+button = 3
+grovepi.pinMode(button,"INPUT")
+
+# Connect the Grove Temperature Sensor to analog port A0
+sensor = 0
+
+# Connect the Grove Rotary Angle Sensor to analog port A1
+potentiometer = 1
+time.sleep(1)
+### Reference voltage of ADC is 5v
+adc_ref = 5
+### Vcc of the grove interface is normally 5v
+grove_vcc = 5
+### Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
+full_angle = 300
+
+########################
+##WELCOME TO SABER CLASS
+########################
 class Saber:
 
     def __init__(self):
-        print "Saber initialized."
+        print "Saber instantiated."
 
-    def blink(self):
+    
+    ######################
+    ######################
+    ### DEMOS
+    ######################
+    ######################
+
+        
+    ############
+    #LCD demo
+    ############    
+    def demoLCD(self):
+        setText("Hello world\nLCD test")
+        setRGB(0,128,64)
+        
+        for c in range(0,255):
+            setRGB(c,255-c,0)
+            time.sleep(0.01)
+        
+        setRGB(0,255,0)
+        setText("Bye bye, this should wrap")
+        
+    ############
+    #Temp demo
+    ############
+    def demoTemp(self):
+        while True:
+            try:
+                temp = grovepi.temp(sensor,'1.1')
+                print("temp =", temp)
+                time.sleep(.5)
+        
+            except KeyboardInterrupt:
+                break
+            except IOError:
+                print ("Error")
+
+    ############
+    #Buzzer demo
+    ############              
+    def demoBuzzer(self):
+        while True:
+            try:
+                # Buzz for 1 second
+                grovepi.digitalWrite(buzzer,1)
+                print ('start')
+                time.sleep(1)
+        
+                # Stop buzzing for 1 second and repeat
+                grovepi.digitalWrite(buzzer,0)
+                print ('stop')
+                time.sleep(1)
+        
+            except KeyboardInterrupt:
+                grovepi.digitalWrite(buzzer,0)
+                break
+            except IOError:
+                print ("Error")
+
+    ############
+    #Button demo
+    ############    
+    def demoButton(self):
+        while True:
+            try:
+                print(grovepi.digitalRead(button))
+                time.sleep(.5)
+        
+            except IOError:
+                print ("Error")
+
+    ############
+    #LED demo
+    ############   
+    def demoLED(self):
         while True:
             try:
                 #Blink the LED
-                digitalWrite(led,1)		# Send HIGH to switch on LED
+                digitalWrite(gled,1)		# Send HIGH to switch on LED
                 print "LED ON!"
                 time.sleep(1)
 
-                digitalWrite(led,0)		# Send LOW to switch off LED
+                digitalWrite(gled,0)		# Send LOW to switch off LED
                 print "LED OFF!"
                 time.sleep(1)
 
             except KeyboardInterrupt:	# Turn LED off before stopping
-                digitalWrite(led,0)
+                digitalWrite(gled,0)
                 break
             except IOError:				# Print "Error" if communication error encountered
                 print ("Error")
 
-
-    def distance(self):
+    ############
+    #US_Range demo
+    ############  
+    def demoRange(self):
         while True:
             try:
                 # Read distance value from Ultrasonic
@@ -45,7 +171,20 @@ class Saber:
             except IOError:
                 print ("Error")
 
-    def proxAlert(self):
+    
+    ######################
+    ######################
+    ### LOGIC
+    ######################
+    ######################
+    
+    
+    
+    ############
+    #Sensor in box checks if something (box's lid) is right 
+    #Above. Triggers alarm if opened. Button disables
+    ############ 
+    def coverCheck(self):
         counter = 0
         while True:
             try:
@@ -57,11 +196,11 @@ class Saber:
                 elif dist < 50:
                     try:
                         #Blink the LED
-                        digitalWrite(led,1)		# Send HIGH to switch on LED
+                        digitalWrite(rled,1)		# Send HIGH to switch on LED
                         print "Wow!"
                         time.sleep(1)
 
-                        digitalWrite(led,0)		# Send LOW to switch off LED
+                        digitalWrite(rled,0)		# Send LOW to switch off LED
                         print "Something's close"
                         time.sleep(1)
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 GrovePi Experiments
 
 ## Things we're working on currently
-    Adding sensors
+    Adding basic GrovePi components
 
 ## Things we want to build
     **Pet robot**
@@ -11,30 +11,36 @@ GrovePi Experiments
     Facial recognition
     User inferface (web or app)
     Text / email messaging
-    Movement
+    Movement / chassis
+    Custom 3D printed electronics housing
     
 ## Things we want to 3D print
     http://www.thingiverse.com/thing:624084
     http://www.thingiverse.com/thing:613083
 
 ## Things we want to buy
-    More cards: http://www.dexterindustries.com/shop/grovepi-board/
+    2 more GrovePi's: http://www.dexterindustries.com/shop/grovepi-board/
     HDMI to VGA adatper (low priority)
     Treds: http://www.amazon.com/dp/B01428VFBS/ref=wl_it_dp_o_pC_nS_ttl
 
 ## Things we want to learn
+    Package management: http://nvie.com/posts/better-package-management/
+        http://www.fullstackpython.com/application-dependencies.html
+    Flask Python Framework
     CAD for RaspberryPi case
-    Why isn't my streaming working?
     How can we connect to an MCU?
     Google Vision Platform
     Wiring relays
-    Flask Python Framework
+
     
 ## Things we used to build Saber
     http://ide.c9.io
     GrovePi
     RaspberryPi 2
+    pistreaming
 
 ## Things we may add later on
-    Quad-copter frame
+    Quad-copter frame: 
+        http://www2.warwick.ac.uk/fac/sci/eng/meng/wmr/projects/autonomousuav/poster_design_final.pdf
+        https://www.raspberrypi.org/magpi-issues/MagPi19.pdf
     GPS positioning
