b7b6bd82c3eb76337da932b02c342a9eaca7983c
dadiletta <dan.adiletta@gmail.com>
2016-05-20 19:39:55 -0400

    Local attempt on Tkinter

--- TkinterWork.py
+++ TkinterWork.py
@@ -34,8 +34,12 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label = tk.Label(self, text="Welcome to Saber", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self, text="Lights",
+                            command = lamda: controller.show_frame(PageOne))
+        button1.pack()
 
 
 app = TkWork()
