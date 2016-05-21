b0837c061f75590dfddd3f6187a00e07ec3bc0ab
Dan Adiletta <dan.adiletta@gmail.com>
2016-05-21 02:24:21 +0000

    Button page switch

--- TkinterWork.py
+++ TkinterWork.py
@@ -16,12 +16,14 @@ class TkinterWork(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-
-        frame = StartPage(container, self)
-
-        self.frames[StartPage] = frame
-
-        frame.grid(row=0, column=0, sticky="nsew")
+        
+        for F in (StartPage, PageOne):
+    
+            frame = F(container, self)
+    
+            self.frames[F] = frame
+    
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
