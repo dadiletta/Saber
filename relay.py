d67919601ec5da3b0863042bed5176291b551766
dadiletta <dan.adiletta@gmail.com>
2016-10-13 17:55:05 -0400

    Added relay

--- relay.py
+++ relay.py
@@ -3,36 +3,40 @@ import RPi.GPIO as GPIO
 import time
 import bluetooth
 
+#BCM is different numbering than BOARD. Google it
 GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
-
+# This makes changing pins less of a chore
 pinList = [14]
 
 # loop through pins and set mode and state to 'high'
-
 for i in pinList:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 
 # time to sleep between operations in the main loop
+SleepTimeL = 15
 
-SleepTimeL = 2
+#counter to double check bluetooth before cutting power
+count = 0
 
 # main loop
-
 try:
   while True:
-      result = bluetooth.lookup_name('08:ec:a9:0f:88:3e', timeout=5)
-      if(result != None):
-          print "I see Dan!"
-      for x in pinList:
-        GPIO.output(x, GPIO.LOW)
-        print str(x) + " set to low"
-        time.sleep(SleepTimeL);
-        GPIO.output(x, GPIO.HIGH)
-        print str(x) + " set to high"
-        time.sleep(SleepTimeL);
+    result = bluetooth.lookup_name('08:ec:a9:0f:88:3e', timeout=5)
+    if(result != None):
+        print "I see Mr. A!"
+        GPIO.output(pinList[0], GPIO.HIGH)
+        print str(pinList[0]) + " set to high"
+        count = 0
+    else:
+        #we see the bluetooth missing once
+        count += 1;
+        if count > 1:
+        GPIO.output(pinList[0], GPIO.LOW)
+    #break between cycles
+    time.sleep(SleepTimeL)
 
   GPIO.cleanup()
   print "Good bye!"
